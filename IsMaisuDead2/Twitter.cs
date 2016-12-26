aef932d4813b0eedd7ee8b99d37952e675910553
Maisu <mystasly48@gmail.com>
2016-12-26 10:03:31 +0900

    Add Twitter Class

---
+++ IsMaisuDead2/Twitter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreTweet;
+
+namespace IsMaisuDead2 {
+  class Twitter {
+    Tokens tokens;
+
+    public Twitter(string ConsumerKey, string ConsumerSecret, string AccessToken, string AccessSecret) {
+      tokens = Tokens.Create(ConsumerKey, ConsumerSecret, AccessToken, AccessSecret);
+    }
+
+    public void Tweet(string text) {
+      tokens.Statuses.Update(status: text);
+    }
+  }
+}
