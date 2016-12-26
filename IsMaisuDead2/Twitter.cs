40754369b01010d53193c79da1129f6104af488e
Maisu <mystasly48@gmail.com>
2016-12-26 11:03:02 +0900

    Add UpdateName

--- IsMaisuDead2/Twitter.cs
+++ IsMaisuDead2/Twitter.cs
@@ -8,13 +8,32 @@ using CoreTweet;
 namespace IsMaisuDead2 {
   class Twitter {
     Tokens tokens;
+    const string ONLINE = " (Online)";
+    const string OFFLINE = " (Offline)";
 
     public Twitter(string ConsumerKey, string ConsumerSecret, string AccessToken, string AccessSecret) {
       tokens = Tokens.Create(ConsumerKey, ConsumerSecret, AccessToken, AccessSecret);
     }
 
-    public void Tweet(string text) {
-      tokens.Statuses.Update(status: text);
+    public void Online() {
+      UpdateStatus(ONLINE);
+    }
+
+    public void Offline() {
+      UpdateStatus(OFFLINE);
+    }
+
+    private void UpdateStatus(string status) {
+      string replaceName = (status == ONLINE) ? OFFLINE : ONLINE;
+      UpdateName(GetName().Replace(replaceName, "") + status);
+    }
+
+    private string GetName() {
+      return tokens.Account.UpdateProfile().Name;
+    }
+
+    private void UpdateName(string name) {
+      tokens.Account.UpdateProfile(name: name);
     }
   }
 }
